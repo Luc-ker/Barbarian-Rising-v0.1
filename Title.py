b5d7e944bb75b79b335db666fc91b2cdb52bbb8a
Jonathan Lee <jlee4889@gmail.com>
2023-09-21 15:18:46 +0100

    Update Title.py

--- Title.py
+++ Title.py
@@ -1,30 +1,48 @@
-import pygame, sys
-from pygame.locals import QUIT, KEYDOWN
+import pygame, sys, time
+from pygame.locals import *
 
-def message(display, message="", text=(0, 255, 0), textBg=(0, 0, 128)):
+pygame.init()
+
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 450
+font = pygame.font.Font('freesansbold.ttf', 32)
+display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+#colours
+green = (0, 255, 0)
+blue = (0, 0, 128)
+orange = (235, 171, 52)
+
+def message(message=""):
     message = input("Enter a message.\n")
-    font = pygame.font.Font('freesansbold.ttf', 32)
-    text = font.render(message, True, text, textBg)
-    display.blit(text,(0,436))
+    text = ''
+    for letter in message:
+        display.fill(orange)
+        text += letter
+        text_surface = font.render(text, True, blue)
+        text_rect = text_surface.get_rect()
+        text_rect.left = 4
+        text_rect.bottom = int(WINDOW_HEIGHT*0.8)
+        display.blit(text_surface, text_rect)
+        pygame.display.update()
+        pygame.time.wait(100)
   
 
-def title():
+def main():
     run = True
-    green = (0, 255, 0)
-    blue = (0, 0, 128)
-    pygame.init()
     score = 0
-    display = pygame.display.set_mode((500, 500))
     pygame.display.set_caption('Barbarian Rising')
     pygame.display.set_icon(pygame.image.load("./Graphics/icon.png"))
     clock = pygame.time.Clock()
-    font = pygame.font.Font('freesansbold.ttf', 32)
+    display.fill(orange)
     text = font.render(f'Score: {score}', True, green, blue)
 
     while run:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
-            message(display)
+            message()
+        if keys[pygame.K_RETURN]:
+            display.fill(orange)
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -35,4 +53,4 @@ def title():
 #overtext = font.render('Press any key to restart.', True, green, blue)
 
 if __name__ == "main":
-    title()
+    main()
